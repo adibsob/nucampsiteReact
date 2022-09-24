9c589d841dfe7b404e267732c2ce6b84a3621faf
adibsob <99847582+adibsob@users.noreply.github.com>
2022-09-24 12:00:47 -0700

    Tiny fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import CampsitesList from './features/campsites/CampsitesList';
-import {CAMPSITES} from './app/shared/CAMPSITES';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
