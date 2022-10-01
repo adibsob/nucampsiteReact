5c2f014ea4790164db0344f5fde9541a4751d2b3
adibsob <99847582+adibsob@users.noreply.github.com>
2022-10-01 13:58:32 -0700

    This should have worked but it doesn't so lets see

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,16 +1,11 @@
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-// export const selectRandomCampsite = () => {
-//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-// }
-// instead let use this:
-
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === id);
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
